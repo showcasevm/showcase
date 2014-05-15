f8b2d859f1853326713072c883114b3fa0d43e4c
Showcase <florian.rietz@novatec-gmbh.de>
2014-05-15 12:38:38 +0200

    dede

---
+++ src/main/java/info/novatec/ci/showcase/Add.java
@@ -0,0 +1,20 @@
+package info.novatec.ci.showcase;
+
+/**
+ * Hello world!
+ *
+ */
+public class Add 
+{
+    public static void main( String[] args )
+    {
+      int result, number_one,number_two;
+      
+      number_one = 1;
+      number_two = 2;
+      
+      result = number_one + number_two;
+      
+      System.out.println(result);
+    }
+}
